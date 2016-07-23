370f54451b1f020d3e871bf8cf78c2fab3ade60c
Lærne (Nicolas Brack) <nicolas.brack@student.uclouvain.be>
2016-07-23 12:30:23 +0200

    Initial extension, only doing a full desaturation.

---
+++ README.md
@@ -0,0 +1,16 @@
+Installation
+============
+
+Copy this folder to the gnome-shell extension folders, typically under `.local/share/gnome-shell/extensions`.
+
+Using a terminal, you can do the following sequence of commands :
+
+$ git clone https://github.com/laerne/desaturate_all.git
+$ mv desaturate_all ".local/share/gnome-shell/extensions/desaturate_all@nicolas.brack.mail.be"
+
+Then restart gnome by pressing `ALT`+`F2`, then entering `r` and validating with `RETURN`.
+
+You can now enable the extension by browsing to https://extensions.gnome.org/local .
+
+
+
---
+++ extension.js
@@ -0,0 +1,56 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+const Clutter = imports.gi.Clutter;
+const St = imports.gi.St;
+const Main = imports.ui.main;
+
+let button;
+let extension_icon;
+let color_effect;
+
+function _toggleEffect() {
+    if ( Main.uiGroup.has_effects( color_effect ) ) {
+        Main.uiGroup.remove_effect( color_effect );
+    } else {
+        Main.uiGroup.add_effect( color_effect );
+    }
+}
+
+
+function init() {
+    //Creation of button
+    button = new St.Bin({ style_class: 'panel-button',
+                          reactive: true,
+                          can_focus: true,
+                          x_fill: true,
+                          y_fill: false,
+                          track_hover: true });
+    extension_icon = new St.Icon({ icon_name: 'applications-graphics-symbolic',
+                                   style_class: 'system-status-icon' });
+    button.set_child(extension_icon);
+
+    //Creation of effect
+    color_effect = new Clutter.DesaturateEffect();
+    
+    //Signal connection
+    button.connect('button-press-event', _toggleEffect);
+}
+
+function enable() {
+    Main.panel._rightBox.insert_child_at_index(button, 0);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_child(button);
+}
